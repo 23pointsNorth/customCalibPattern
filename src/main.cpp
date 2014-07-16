10471e55788734904cbcefeaffeb8760448663ed
Daniel Angelov <dani_angelov@yahoo.com>
2014-07-16 14:44:15 +0300

    Measuing chessboard points accuracy.

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,33 @@
 using namespace cv;
 using namespace std;
 
-#define	SQUARE_SIZE_M 0.035
+#define	SQUARE_SIZE_M 0.024 //m
+
+double computeReprojectionErrors( const vector<vector<Point3f> >& objectPoints,
+                                         const vector<vector<Point2f> >& imagePoints,
+                                         const vector<Mat>& rvecs, const vector<Mat>& tvecs,
+                                         const Mat& cameraMatrix , const Mat& distCoeffs,
+                                         vector<float>& perViewErrors)
+{
+    vector<Point2f> imagePoints2;
+    int i, totalPoints = 0;
+    double totalErr = 0, err;
+    perViewErrors.resize(objectPoints.size());
+
+    for( i = 0; i < (int)objectPoints.size(); ++i )
+    {
+        projectPoints( Mat(objectPoints[i]), rvecs[i], tvecs[i], cameraMatrix,
+                       distCoeffs, imagePoints2);
+        err = norm(Mat(imagePoints[i]), Mat(imagePoints2), NORM_L2);
+
+        int n = (int)objectPoints[i].size();
+        perViewErrors[i] = (float) std::sqrt(err*err/n);
+        totalErr        += err*err;
+        totalPoints     += n;
+    }
+
+    return std::sqrt(totalErr/totalPoints);
+}
 
 void onMouse(int event, int x, int y, int, void* data)
 {
@@ -34,6 +60,59 @@ void onMouse(int event, int x, int y, int, void* data)
     }
 }
 
+double total_mean = 0;
+int tcount = 0;
+
+void chessboard_accuracy(Mat& image)
+{
+	vector<Point2f> corners;
+	Size boardSize(9, 6);
+	bool patternfound = findChessboardCorners(image, boardSize, corners,
+            CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE + CALIB_CB_FAST_CHECK);
+
+	if (!patternfound) return;
+#if 1
+	Mat gray;
+	cvtColor(image, gray, COLOR_RGB2GRAY);
+	cornerSubPix(gray, corners, Size(11, 11), Size(-1, -1),
+                    TermCriteria(CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, 30, 0.1));
+#endif
+	// create std points
+	int squareSize = 10;
+	vector<Point2f> board;
+	 for( int i = 0; i < boardSize.height; ++i )
+            for( int j = 0; j < boardSize.width; ++j )
+                board.push_back(Point2f(float( j*squareSize ), float( i*squareSize )));
+
+
+	Mat H = findHomography(board, corners, RANSAC);
+    if (H.empty())
+    {
+        cout << "findHomography() returned empty Mat." << endl;
+        return;
+    }
+
+    vector<Point2f> proj;
+    perspectiveTransform(board, proj, H);
+
+    double sum = 0;
+    for (uint i = 0; i < proj.size(); ++i)
+    {
+    	double error = norm(proj[i] - corners[i]);
+    	cout << error << endl;
+    	sum += error;
+    }
+
+    cout << "Total error: " << sum << " over " << board.size() << " points." << endl;
+    cout << "Mean error: " << sum / board.size() << endl;
+
+    cout << "-----------" << endl;
+    total_mean += (sum/board.size());
+    tcount++;
+    cout << "Total mean:" << total_mean / tcount << endl;
+    cout << "-----------" << endl;
+}
+
 int main()
 {
 	CustomPattern* pattern;
@@ -51,6 +130,9 @@ int main()
 		rectangle(canvas, roi, CV_RGB(255, 0, 0));
 		imshow("Select Pattern", canvas);
 		key = waitKey(10);
+
+		chessboard_accuracy(frame);
+
 	}while(key != 't');
 	destroyWindow("Select Pattern");
 
@@ -90,12 +172,31 @@ int main()
 	cout << "RMS: " << pattern->calibrate(obj_points, matched_points, frame.size(), K, distCoeff, rvec, tvec) << endl;
 	cout << "K: " << K << endl << "distCoeff" << distCoeff << endl;
 
+	FileStorage fs("laptop_webcam_output.xml",  FileStorage::READ);
+	fs["distortion_coefficients"] >> distCoeff;
+	vector<float> perViewErrors;
+	cout << "Alternatively Computed rms" << computeReprojectionErrors(obj_points,
+                                         matched_points,
+                                         rvec, tvec,
+                                         K, distCoeff,
+                                         perViewErrors) << endl;
+
+
 	Mat undist;
 	undistort(frame, undist, K, distCoeff);
-	imshow("Undistorted", undist);
 	imwrite("undistorted.png", undist);
 	imwrite("frame.png", frame);
-	waitKey();
+
+	do
+	{
+		video >> frame;
+		undistort(frame, undist, K, distCoeff);
+		imshow("Undistorted", undist);
+		key = waitKey(10);
+	}while(key != 'q');
 
 	return 0;
 }
+
+
+
